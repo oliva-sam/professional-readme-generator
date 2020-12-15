cbbfb4e087cc481f2141acc18bb5a3d344a65186
Melody Monarch <oliva.sam.so@gmail.com>
2020-12-15 13:05:44 -0600

    connected js files and added info to generate readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,16 @@
 const inquirer = require("inquirer");
+const utils = require('./utils/generateMarkdown');
 
 // array of questions for user
 const questions = [
-    "What is your GitHub username?",
-    "What is your email address?",
-    "What is your project's name?",
-    "What kind of license should you have?",
-    "What command should be run to initialize tests?",
-    "What does the user need to know about using the repo?",
-    "What does the user need to know about contributing to the repo?"
+    "0. What is your GitHub username?",
+    "1. What is your email address?",
+    "2. What is your project's name?",
+    "3. What kind of license should you have?",
+    "4. What command should be run to install dependencies?",
+    "5. What command should be run to initialize tests?",
+    "6. What does the user need to know about using the repo?",
+    "7. What does the user need to know about contributing to the repo?"
 ];
 
 // function to write README file
@@ -17,7 +19,58 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer
+    .prompt([
+        {
+            name: "username",
+            type: "input",
+            message: questions[0]
+        },
+        {
+            name: "email",
+            type: "input", 
+            message: questions[1]
+        },
+        {
+            name: "project_name",
+            type: "input", 
+            message: questions[2]
+        }, 
+        {
+            name: "license",
+            type: "list", 
+            message: questions[3],
+            choices: ["MIT", "Apache 3.0", "BSD 3", "GDL 3.0", "None"],
+            default: "None"
+        }, 
+        {
+            name: "dependencies_command",
+            type: "input", 
+            message: questions[4],
+            default: "npm i"
+        },
+        {
+            name: "test_command",
+            type: "input", 
+            message: questions[5],
+            default: "manual test"
+        },
+        {
+            name: "about_repo",
+            type: "input", 
+            message: questions[6]
+        },
+        {
+            name: "contribute_repo",
+            type: "input", 
+            message: questions[7],
+            default: "Samantha Oliva"
+        },
+    ])
+    .then((answer) => {
+        let readMetext = utils(answer)
+        console.log(readMetext)
+    });
 }
 
 // function call to initialize program
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,9 +2,43 @@ const fs = require('fs');
 
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(data)
+  let readMe = `
+# ${data.project_name}
+## Description
+${data.about_repo}
+### Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributors](#contributors)
+* [License](#license)
+* [Test](#test)
+* [Contact](contact)
 
-`;
+## Installation
+${data.dependencies_command}
+
+## Usage
+gif or screenshot here
+
+## Contributors
+${data.contribute_repo}
+
+## License
+${data.license} 
+![GitHub license](https://img.shields.io/badge/license-${data.license}-blue.svg)
+
+## Test
+${data.test_command}
+
+## Contact
+If you have any questions feel free to reach me at
+${data.email}
+
+  `
+  return readMe
+
+;
 }
 
 module.exports = generateMarkdown;
