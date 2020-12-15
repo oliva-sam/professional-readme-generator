a107edace4fd6b772f83effc29b88af78a6f0e73
Melody Monarch <oliva.sam.so@gmail.com>
2020-12-15 12:06:42 -0600

    questions to ask user added to array

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,14 @@
+const inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
-
+    "What is your GitHub username?",
+    "What is your email address?",
+    "What is your project's name?",
+    "What kind of license should you have?",
+    "What command should be run to initialize tests?",
+    "What does the user need to know about using the repo?",
+    "What does the user need to know about contributing to the repo?"
 ];
 
 // function to write README file
